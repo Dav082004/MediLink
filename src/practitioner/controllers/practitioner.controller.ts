d33ddb9eb4cf659fc619c59e011e537b0e0e1125
nicodiez02 <nicopdiez01@gmail.com>
2025-10-10 21:12:00 -0300

    feat: CREATE endpoint

--- src/practitioner/controllers/practitioner.controller.ts
+++ src/practitioner/controllers/practitioner.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { RolesTypes } from 'src/auth/auth.types';
 import { Roles } from 'src/auth/roles/roles.decorator';
 import { RolesGuard } from 'src/auth/roles/roles.guard';
 
 @Controller('practitioner')
 export class PractitionerController {
   @UseGuards(AuthGuard('jwt'), RolesGuard)
-  @Roles('medic')
+  @Roles(RolesTypes.ADMIN)
   @Post('register-practitioner')
   public async create() {
-    return 'chetoo';
+    return 'registrar';
   }
 }
